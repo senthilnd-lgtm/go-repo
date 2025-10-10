9b336e4e8327be690ef9b814ef16d5850ea363b8
senthilnd-lgtm <senthilnd@gmail.com>
2025-10-10 21:15:07 +0530

    add sub function

--- HelloWorld.go
+++ HelloWorld.go
@@ -11,6 +11,10 @@ func add(a, b int) (x, y int) {
 	return
 }
 
+func sub(a, b int) int {
+	return a - b
+}
+
 func main() {
 	num1 := 5
 	num2 := 7
@@ -19,6 +23,7 @@ func main() {
 	fmt.Println(add(num1, num2))
 	fmt.Println(math.Pi)
 
+	fmt.Println(sub(num1, num2))
 	const myconst = 1
 	fmt.Printf("type : %T, value :%v", myconst+1.1, myconst+1.1)
 
