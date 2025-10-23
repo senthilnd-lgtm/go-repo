3c9f3c5af3359d207c6793c66f0c8dccc25eaced
senthilnd-lgtm <senthilnd@gmail.com>
2025-10-23 17:30:04 +0530

    added additional func code

--- Functions/func.go
+++ Functions/func.go
@@ -32,6 +32,29 @@ func namedReturn(a, b int) (sum int, diff int) {
 	return
 }
 
+func vara(st string, ar ...int) {
+	fmt.Println(st)
+	for id, val := range ar {
+		fmt.Println(id, val)
+	}
+}
+
+func argfunc(x func()) {
+	x()
+}
+
+func ret() func() {
+	return func() { fmt.Println("Function return") }
+}
+
+func makeCounter() func() int {
+	count := 0
+	return func() int {
+		count++
+		return count
+	}
+}
+
 func main() {
 	sayHello()
 	greet("Senthil")
@@ -46,4 +69,31 @@ func main() {
 	namedSum, namedDiff := namedReturn(10, 5)
 	fmt.Println("Named Return - Sum:", namedSum, "Diff:", namedDiff)
 	fmt.Println(namedReturn(20, 10))
+
+	a := 10
+	func() {
+		fmt.Println("Func calling")
+	}()
+
+	k := func(x int) {
+		fmt.Println("Func var ass", a, x)
+	}
+
+	k(20)
+
+	vara("Senthil", 1, 2, 3, 5)
+
+	retfunc := ret()
+	retfunc()
+
+	argfunc(retfunc)
+
+	// closure and state
+
+	c1 := makeCounter()
+	c2 := makeCounter()
+
+	fmt.Println(c1()) // 1
+	fmt.Println(c1()) // 2
+	fmt.Println(c2()) // 1
 }
