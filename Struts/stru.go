0d5843288d497c5aac30f992aca469d34e05cf37
senthilnd-lgtm <senthilnd@gmail.com>
2025-11-26 12:01:50 +0530

    added add code

--- Struts/stru.go
+++ Struts/stru.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 )
 
 // Strut represents a basic structure for demonstration.
@@ -9,6 +10,18 @@ type Strut struct {
 	ID    int
 	Name  string
 	Value float64
+}
+
+type Account struct {
+	AccountNum int
+	Balance    int
+	p          Personal
+}
+
+type Personal struct {
+	Name  string
+	Email string
+	Phone int
 }
 
 // NewStrut creates a new Strut instance.
@@ -35,10 +48,41 @@ func (s *Strut) UpdateName(newName string) {
 	s.Name = newName
 }
 
+func (a *Account) DisplayAccInfo() {
+	fmt.Println("From method", a.AccountNum, a.Balance, a.p.Name)
+
+}
+
 func main() {
 	s := NewStrut(1, "Example", 42.0)
 	s.Display()
 	s.SetValue(100.5)
 	s.UpdateName("Updated Example")
 	s.Display()
+
+	var a Account
+	a.AccountNum = 20
+	a.Balance = 100
+	fmt.Println(a)
+
+	var aa = Account{AccountNum: 10, p: Personal{Name: "Sen"}}
+	fmt.Println(aa)
+	aa.DisplayAccInfo()
+
+	fmt.Println(" TYpe of acc", reflect.TypeOf(aa))
+	fmt.Println(" Value of acc", reflect.ValueOf(aa))
+
+	var aaa = Account{}
+	fmt.Println(aaa)
+
+	if aa == aaa {
+		fmt.Println("aa == aaa")
+	} else {
+		fmt.Println("aa != aaa")
+	}
+
+	var aaaa = new(Account)
+	aaaa.AccountNum = 324
+	aaaa.Balance = 1000
+	fmt.Println(*aaaa)
 }
